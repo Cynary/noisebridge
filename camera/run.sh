32404ba8488f22956107e24c7be2ebdd3c17657d
Rodrigo Toste Gomes <rgomes@codeblurb.com>
2019-04-14 13:44:02 -0700

    Improvements to run script, and debug image, as well as fixes for older docker versions (turns out you need to put a trailing '/' at the end of directory names

--- camera/run.sh
+++ camera/run.sh
@@ -1,17 +1,40 @@
 #!/bin/sh
 
-# Lots of help from http://wiki.ros.org/docker/Tutorials/GUI
-#
-XSOCK=/tmp/.X11-unix
-XAUTH=/tmp/.docker.xauth
-touch $XAUTH
+SCRIPT=$(readlink -f $0)
+SCRIPT_PATH=$(dirname "$SCRIPT")
+cd $SCRIPT_PATH
 
-xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+EXTENSION=""
+if [ -f ./Dockerfile.$1 ]
+then
+    EXTENSION=".$1"
+
+    echo "Running image tagged chdkptp$EXTENSION"
+    echo "If you wished to run the $1 command in chdkptp, then run $0 -- $@ instead."
+
+    shift
+fi
+
+if [ "$1" = "--" ]
+then
+    shift
+fi
+
+if ! [ -z "$DISPLAY" ]
+then
+    # Lots of help from http://wiki.ros.org/docker/Tutorials/GUI
+    #
+    XSOCK=/tmp/.X11-unix
+    XAUTH=/tmp/.docker.xauth
+    touch $XAUTH
+
+    xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+fi
 
 exec docker run -it --rm --privileged \
                 --volume=$XSOCK:$XSOCK:rw \
                 --volume=$XAUTH:$XAUTH:rw \
                 --env="XAUTHORITY=${XAUTH}" \
                 --env="DISPLAY" \
-            chdkptp \
+            chdkptp"$EXTENSION" \
             $@
